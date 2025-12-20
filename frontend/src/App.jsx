77a48c98c17789298990234659f0bbba825346fc
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-12-20 22:07:29 +0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      hi
+    <div className="min-h-screen bg-white dark:bg-dark-bg text-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Projects />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
